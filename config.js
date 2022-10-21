99d20ff8e5664be376cac60b481231af2f23a450
Neyzz21 <107815428+Neyzz21@users.noreply.github.com>
2022-10-21 14:41:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
-global.author = 'Naze'
+global.owner = ['6285875063235']
+global.ownernomer = "6285875063235"
+global.premium = ['6285875063235']
+global.packname = 'Sticker  Zar'
+global.author = 'Neyzz'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.prefa = ['','!','.','#','&']
 global.sp = ''
